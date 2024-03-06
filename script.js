cf29cd868f27a324a0b2471b06418234447e17ff
Agnar Holst <agnarholst@gmail.com>
2024-03-07 00:48:35 +0100

    Initial commit.

---
+++ script.js
@@ -0,0 +1,12 @@
+const b1r1 = document.getElementById("b1r1");
+const b1r2 = document.getElementById("b1r2");
+const b1r3 = document.getElementById("b1r3");
+const b1r4 = document.getElementById("b1r4");
+const b1r5 = document.getElementById("b1r5");
+const b1r6 = document.getElementById("b1r6");
+const b1r7 = document.getElementById("b1r7");
+const b1r8 = document.getElementById("b1r8");
+const b1r9 = document.getElementById("b1r9");
+
+
+b1r9.innerHTML = "9";
