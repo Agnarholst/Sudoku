a8a4684efcb700b6e18b2cf95446cde18cddd747
Agnar Holst <agnarholst@gmail.com>
2024-03-08 13:00:55 +0100

    Added numbered buttons 1 to 9. And interactivity so when a cell is clicked it becomes active and a numbered button can be clicke to get the same number in the active cell

--- script.js
+++ script.js
@@ -1,12 +1,39 @@
-const b1r1 = document.getElementById("b1r1");
-const b1r2 = document.getElementById("b1r2");
-const b1r3 = document.getElementById("b1r3");
-const b1r4 = document.getElementById("b1r4");
-const b1r5 = document.getElementById("b1r5");
-const b1r6 = document.getElementById("b1r6");
-const b1r7 = document.getElementById("b1r7");
-const b1r8 = document.getElementById("b1r8");
-const b1r9 = document.getElementById("b1r9");
+// const b1r1 = document.getElementById("b1r1");
+// const b1r2 = document.getElementById("b1r2");
+// const b1r3 = document.getElementById("b1r3");
+// const b1r4 = document.getElementById("b1r4");
+// const b1r5 = document.getElementById("b1r5");
+// const b1r6 = document.getElementById("b1r6");
+// const b1r7 = document.getElementById("b1r7");
+// const b1r8 = document.getElementById("b1r8");
+// const b1r9 = document.getElementById("b1r9");
 
 
-b1r9.innerHTML = "9";
+// b1r9.innerHTML = "9";
+
+// When b1r9 is clikced make able to changed innerHTML.
+// if  between 1 and 9 = change innerHTML
+// else Invalid - try againg
+
+// const what = document.getElementById("b1r1").onclick = function () {
+//     what.innerHTML = "9";
+// }
+
+let activeCell = null;
+
+function makeActive(cell) {
+    if (activeCell !== null) {
+        activeCell.classList.remove("active");
+    }
+
+    activeCell = cell;
+    cell.classList.add("active");
+}
+
+
+function setNumber(num) {
+    //active cell will get the number of button clicked. 
+    if (activeCell !== null) {
+        activeCell.innerHTML = num;
+    }
+}
