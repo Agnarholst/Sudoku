0ac308c3f964a1717c3d9c87ec60f997dae66516
Agnar Holst <agnarholst@gmail.com>
2024-03-12 10:03:26 +0100

    I added a Object to check if chosen number was correct, three fails in a row an a message tells player Game Over. Board expanded from 9 cells to 81 cells. Each cell has a ID-number. Did some minor styling.

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-
-
-
-
-
 let activeCell = null;
 let activeCellId = null;
 let box = [];
-console.log(box);
 const sudokuboard = {
-    1: "2",
-    2: "9",
-    3: "1",
-    4: "5",
-    5: "3",
-    6: "8",
-    7: "4",
-    8: "7",
-    9: "6"
+    "1": 5, "2": 0, "3": 3, "4": 0, "5": 2, "6": 6, "7": 4, "8": 9, "9": 0,
+
+    "10": 9, "11": 1, "12": 0, "13": 0, "14": 3, "15": 0, "16": 0, "17": 8, "18": 6,
+
+    "19": 0, "20": 0, "21": 7, "22": 8, "23": 0, "24": 5, "25": 2, "26": 0, "27": 0,
+
+    "28": 0, "29": 5, "30": 0, "31": 7, "32": 0, "33": 0, "34": 8, "35": 0, "36": 9,
+
+    "37": 0, "38": 2, "39": 0, "40": 0, "41": 5, "42": 0, "43": 0, "44": 0, "45": 0,
+
+    "46": 1, "47": 7, "48": 8, "49": 0, "50": 0, "51": 3, "52": 0, "53": 0, "54": 2,
+
+    "55": 0, "56": 0, "57": 0, "58": 0, "59": 7, "60": 8, "61": 0, "62": 4, "63": 5,
+
+    "64": 0, "65": 0, "66": 0, "67": 0, "68": 4, "69": 0, "70": 7, "71": 9, "72": 0,
+
+    "73": 0, "74": 2, "75": 4, "76": 0, "77": 1, "78": 9, "79": 3, "80": 0, "81": 0
 };
 
 function makeActive(cell) {
@@ -27,6 +29,28 @@ function makeActive(cell) {
     activeCell = cell;
     cell.classList.add("active");
 }
+
+let wrongCounter = 0;
+
+function setNumber(num) {
+    activeCellId = activeCell.getAttribute("id"); //Et eller annet for fetche id'en til elementet. 
+    if (num === sudokuboard[activeCellId]) {
+        activeCell.innerHTML = num;
+        box.push(num);
+        console.log(box);
+    } else {
+        wrongCounter++
+        console.log("Wrong number.")
+    }
+    if (wrongCounter >= 3) {
+        console.log("You lost.")
+    }
+}
+
+
+// Funksjon som gjør at alle ruter med tallet 0 er "usynlige" så 0 ikke vises. 
+
+
 
 // Checks if box is full against an array
 // function setNumber(num) {
@@ -42,37 +66,22 @@ function makeActive(cell) {
 //         box.push(num);
 //         console.log(box);   
 //     }
-// }
 
-// Checks if box is full against an object which is the Solution. 
-function setNumber(num) {
-    console.log(activeCell)
-    activeCellId = activeCell.getAttribute("id"); //Et eller annet for fetche id'en til elementet. 
-    console.log("Id:", activeCellId)
-    console.log("Number:", num)
-    if (activeCell !== null) {
-        console.log("SetNumber Function Start")
-        // Sjekk opp mot key i 
-        for (let i of box) {
-            console.log(i)
-            if (num == i) {
-                return console.log("Break. Already a number in the box.");
-            }
-        }
-        activeCell.innerHTML = num;
-        box.push(num);
-        console.log(box);   
-    }
-}
+// Sudokubrett kan jo tenkes som et stort array laget av av 9 arrays... også må man sammenligne arrays...
+// Type eller så har man 18 unike arrays. Virker unødvendig. Eller så har man et array med mye loggikk, og splicing. 
+// Type 81 tall... 1 til 9 må være unike right... 10 til 18 må være unike også videre... da har man jo sjekket at for 
+// radene... også kan man sjekke for en rute.... som sier at rute-id 1, 2, 3 og 10, 11, 12, og 19, 20, 21 må være unike. 
+// Slik sjekker man at boksen ikke har mer enn et av samme tall også må man sjekke rute-id 1, 10, 19, 28 også videre for å
+// sjekke nedover. Det viktigste er at den ikke tar en slik sjekk for alle mulige tall... da blir det uffektivt... så basert
+// på activeCellId som en parameter så må den kunne sjekke de tre greiene, men basert på samme funksjon... eller tre funksjoner. 
+// En for boks, en for vertikal lengde, og en for horisontal lengde. Hvis ikke den gjør kun de tre så vil den jo kjøre alle radene
+// horisontalt, vertikalt, og alle 9 boksene... for å kun skrive inn et tall... 
 
-// Vi trenger å få inn to argumenter... både rute-id som spiller har valgt og nummeren den trykker inn. Nummer har vi jo men vi må gi
-// rute-id til hver rute og hente det på et vis. 
+// Jeg tror det blir å lage en aktiveringsfunksjon akkurat som activeCell... så trigger activeCell en funskjon som aktiverer de 
+// følgende id-rutene... horisontal, vertikal, og boksen activeCell befinner seg i. Også kjøre den de relativt enkel logiske testene.
+// Som blir tre forskjellige if-statements basert på 3 arrays... som er splicet fra hoved array... I guess... også sjekker den en etter 
+// en. Er boks ok? Er horisontal rad ok? Er vertikal rad ok? Hvis disse tre er ok så får man skrive inn ønkelig tall. 
 
-// Når man trykker på html-button elementet så går jo automatisk funksjonen dit med et tall... hardcoda til den knappen. 
-// Men knappen kan jo ikke vite hvilken rute som skal bli trykket. Så jeg må på en måte hente id til activeCell hvis det går an. 
-
-// document.getElementById("demo").innerHTML = "Hello World!";
-
-// Så nå istedenfor for loopen så må jeg jeg bare gjøre et søk på Sudoku objektet basert på Key, altså Id-nummer til rute. 
-// Også sammenligner man num med value... og da skal dette funke ganske bra. Så hvis det er likt! Da skal man få lov til å 
-// skrive tallen i ruten og det følgende key-value-paret tas vekk fra objektet sudokuBoard.
+// Så føler jeg å lage tre arrays for hver gang et tall skal skrives inn er unødvendig?? Kanskje det hadde vært best med med en
+// to dimmensjonal array... et stort array med 9 arrays inni med 9 tall i hver. Da kan man jo bruke indeksering for å si hvilken rad
+// aka array som skal sjekkes. Men må jo drive med splicing der også.... jeg prøver meg først på et array...aka et objekt. 
