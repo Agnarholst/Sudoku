a34abcf1b06e11e96a7c6bc8d4c4ddf69b28fb18
Agnar Holst <agnarholst@gmail.com>
2024-03-13 09:44:13 +0100

    Added a function that sets the board with one sudoku puzzle, and gives numbers that are not 0a color so they can be seen. This way theres not a bunch of zeros around the board.

--- script.js
+++ script.js
@@ -2,70 +2,93 @@ let activeCell = null;
 let activeCellId = null;
 let box = [];
 const sudokuboard = {
-    "1": 5, "2": 0, "3": 3, "4": 0, "5": 2, "6": 6, "7": 4, "8": 9, "9": 0,
+    "1": 5, "2": 0, "3": 3, "4": 9, "5": 1, "6": 0, "7": 0, "8": 0, "9": 7,
 
-    "10": 9, "11": 1, "12": 0, "13": 0, "14": 3, "15": 0, "16": 0, "17": 8, "18": 6,
+    "10": 0, "11": 2, "12": 6, "13": 0, "14": 3, "15": 0, "16": 8, "17": 0, "18": 5,
 
-    "19": 0, "20": 0, "21": 7, "22": 8, "23": 0, "24": 5, "25": 2, "26": 0, "27": 0,
+    "19": 4, "20": 9, "21": 0, "22": 0, "23": 8, "24": 6, "25": 2, "26": 0, "27": 0,
 
-    "28": 0, "29": 5, "30": 0, "31": 7, "32": 0, "33": 0, "34": 8, "35": 0, "36": 9,
+    "28": 0, "29": 5, "30": 0, "31": 0, "32": 2, "33": 0, "34": 1, "35": 7, "36": 8,
 
-    "37": 0, "38": 2, "39": 0, "40": 0, "41": 5, "42": 0, "43": 0, "44": 0, "45": 0,
+    "37": 7, "38": 0, "39": 0, "40": 0, "41": 5, "42": 0, "43": 0, "44": 0, "45": 3,
 
-    "46": 1, "47": 7, "48": 8, "49": 0, "50": 0, "51": 3, "52": 0, "53": 0, "54": 2,
+    "46": 8, "47": 0, "48": 9, "49": 0, "50": 0, "51": 0, "52": 0, "53": 0, "54": 2,
 
-    "55": 0, "56": 0, "57": 0, "58": 0, "59": 7, "60": 8, "61": 0, "62": 4, "63": 5,
+    "55": 0, "56": 0, "57": 0, "58": 0, "59": 0, "60": 0, "61": 0, "62": 2, "63": 4,
 
-    "64": 0, "65": 0, "66": 0, "67": 0, "68": 4, "69": 0, "70": 7, "71": 9, "72": 0,
+    "64": 0, "65": 7, "66": 8, "67": 0, "68": 4, "69": 0, "70": 0, "71": 1, "72": 9,
 
-    "73": 0, "74": 2, "75": 4, "76": 0, "77": 1, "78": 9, "79": 3, "80": 0, "81": 0
+    "73": 0, "74": 4, "75": 5, "76": 7, "77": 9, "78": 0, "79": 3, "80": 0, "81": 0
 };
+
+
+// Forsøk bare å sett første rute til første veriden i nøkkel 1. Også prøv 2-3 ruter, også prøv å lag en loop som tar alle 81. 
+
+
+function setBoard(board) {
+    i = 1;
+    while (i < 81) {
+        cellValue = board[i];
+        console.log(cellValue);
+        if (cellValue !== 0) {
+            document.getElementById(i).style.color = "rgb(228, 234, 253)";
+        }
+        document.getElementById(i).innerHTML = cellValue;
+        i++;
+    }
+}
+
+setBoard(sudokuboard);
+
+function changeColor() {
+
+}
 
 function makeActive(cell) {
     if (activeCell !== null) {
         activeCell.classList.remove("active");
     }
-
     activeCell = cell;
     cell.classList.add("active");
 }
 
 let wrongCounter = 0;
 
-function setNumber(num) {
-    activeCellId = activeCell.getAttribute("id"); //Et eller annet for fetche id'en til elementet. 
-    if (num === sudokuboard[activeCellId]) {
-        activeCell.innerHTML = num;
-        box.push(num);
-        console.log(box);
-    } else {
-        wrongCounter++
-        console.log("Wrong number.")
-    }
-    if (wrongCounter >= 3) {
-        console.log("You lost.")
-    }
-}
+// function setNumber(num) {
+//     activeCellId = activeCell.getAttribute("id"); //Et eller annet for fetche id'en til elementet. 
+//     if (num === sudokuboard[activeCellId]) {
+//         activeCell.innerHTML = num;
+//         box.push(num);
+//         console.log(box);
+//     } else {
+//         wrongCounter++
+//         console.log("Wrong number.")
+//     }
+//     if (wrongCounter >= 3) {
+//         console.log("You lost.")
+//     }
+// }
 
 
 // Funksjon som gjør at alle ruter med tallet 0 er "usynlige" så 0 ikke vises. 
 
 
 
-// Checks if box is full against an array
-// function setNumber(num) {
-//     if (activeCell !== null) {
-//         console.log("SetNumber Function Start")
-//         for (let i of box) {
-//             console.log(i)
-//             if (num == i) {
-//                 return console.log("Break. Already a number in the box.");
-//             }
-//         }
-//         activeCell.innerHTML = num;
-//         box.push(num);
-//         console.log(box);   
-//     }
+//Checks if box is full against an array
+function setNumber(num) {
+    if (activeCell !== null) {
+        console.log("SetNumber Function Start")
+        for (let i of box) {
+            console.log(i)
+            if (num == i) {
+                return console.log("Break. Already a number in the box.");
+            }
+        }
+        activeCell.innerHTML = num;
+        box.push(num);
+        console.log(box);
+    }
+}
 
 // Sudokubrett kan jo tenkes som et stort array laget av av 9 arrays... også må man sammenligne arrays...
 // Type eller så har man 18 unike arrays. Virker unødvendig. Eller så har man et array med mye loggikk, og splicing. 
